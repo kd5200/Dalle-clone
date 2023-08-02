1b0af6e40fe18764a43d7b27a9fa093c36a7c3f6
kd5200 <kareemdavis18@yahoo.com>
2023-08-02 10:08:33 -0400

    client side updates3

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalles", dalleRoutes);
 
-app.get("/hello", async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).json({
     message: "Hello from DALL.E!",
   });
